2eec45b41136263a5b735775e75c97f272f3ffb8
beercafeguy <hemchandra@outlook.com>
2024-04-27 22:42:27 +0530

    Added basic data preprocessing for RNN

---
+++ RNN/LSTM/google_stock_trend.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 27 22:06:41 2024
+
+@author: hemch
+"""
+
+# Recurrent Neural Network
+
+
+
+# Data Processing
+
+
+# Building RNN
+
+
+# Run the predictions
